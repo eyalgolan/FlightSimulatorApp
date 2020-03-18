3092454d3a944bfbe75722696dab98ff43163db8
Eyal Golan <eyalgolan96@gmail.com>
2020-03-18 17:28:57 +0200

    reorginize to mvvm

--- Views/MapControl.xaml.cs
+++ Views/MapControl.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FlightSimulatorApp.controls
+namespace FlightSimulatorApp.Views
 {
     /// <summary>
     /// Interaction logic for MapControl.xaml
