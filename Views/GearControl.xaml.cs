56d0b1b79bc7618eaa36b859fbac79b55e4681f3
YairHanimov <hthtr0@gmail.com>
2020-03-22 20:47:15 +0200

    test mause event

--- Views/GearControl.xaml.cs
+++ Views/GearControl.xaml.cs
@@ -23,24 +23,30 @@ namespace FlightSimulatorApp.Views
         public GearControl()
         {
             InitializeComponent();
+
         }
         private void centerKnob_Completed(object sender, EventArgs e) { }
         private Point fpoint = new Point();
         private void Knob_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            Console.WriteLine("Knob_MouseDown");
+
             if (e.ChangedButton == MouseButton.Left) { fpoint = e.GetPosition(this); }
         }
 
         private void Knob_MouseUp(object sender, MouseButtonEventArgs e)
         {
-          //  knobPosition.X = x;
-           // knobPosition.Y = y;
+            Console.WriteLine("Knob_MouseUp");
+              knobPosition.X = fpoint.X;
+             knobPosition.Y = fpoint.Y;
         }
 
         private void Knob_MouseMove(object sender, MouseEventArgs e)
         {
+
             if (e.LeftButton==MouseButtonState.Pressed)
             {
+
                 double x = e.GetPosition(this).X - fpoint.X;
                 double y = e.GetPosition(this).Y - fpoint.Y;
                 if (Math.Sqrt(x*x + y*y)< base.Width / 2)
