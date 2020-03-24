47d0402ac0696147e16f07d5931d44892449d330
YairHanimov <hthtr0@gmail.com>
2020-03-24 13:33:55 +0200

    jostic

--- Views/GearControl.xaml.cs
+++ Views/GearControl.xaml.cs
@@ -98,7 +98,13 @@ namespace FlightSimulatorApp.Views
             }
 
         }
-
+        private void Knob_Mouseleave(object sender, MouseEventArgs e)
+        {
+            knobPosition.X = 0;
+            knobPosition.Y = 0;
+            rudder = oldx / 60;
+            elevator = -oldy / 60;
+        }
         private void Slider_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
 
@@ -113,6 +119,8 @@ namespace FlightSimulatorApp.Views
         {
 
         }
+
+      
     }
 
 }
